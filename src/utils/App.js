a8d3a498628a2010df988713afd7131ee22cc319
claudia-j-yile <cshao@alumni.risd.edu>
2022-11-13 18:33:04 -0500

    set up file directory

---
+++ src/utils/App.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import About from './components/About';
+import Contact from './components/Contact';
+import Nav from './components/Nav';
+import Portfolio from './components/Portfolio';
+
+function App(){
+    
+}
