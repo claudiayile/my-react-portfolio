895a11ad38280431f4a39785500fe0215907d6be
claudia-j-yile <cshao@alumni.risd.edu>
2022-11-20 18:03:30 -0500

    added index.js

--- src/utils/App.js
+++ src/utils/App.js
@@ -1,9 +1,8 @@
 import React, { useState } from 'react';
 import About from './components/About';
 import Contact from './components/Contact';
-import Nav from './components/Nav';
 import Portfolio from './components/Portfolio';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
