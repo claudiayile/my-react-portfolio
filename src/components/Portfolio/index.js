7484fad20927b232d8bb446d5ed8f12066b82bd7
claudia-j-yile <cshao@alumni.risd.edu>
2022-11-20 17:57:07 -0500

    added Portfolio Component

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,1 +1,15 @@
 import React from 'react';
+
+const Portfolio = ()=>{
+    return(
+        <div>
+            <h1>Portfolio</h1>
+            <p><a href="http://pawshop.herokuapp.com/posts">Paw Shop</a>
+            <a href="https://morning-caverns-82636.herokuapp.com//">Mad Libs</a>
+            <a href="https://noah35becker.github.io/astrosounds/">Astrosounds</a>
+            </p>
+        </div>
+    )
+}
+
+export default Portfolio;
--- src/utils/App.js
+++ src/utils/App.js
@@ -9,19 +9,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
-      </header>
+        <About></About>
+        <Contact></Contact>
+        <Portfolio></Portfolio>
     </div>
   );
 }
